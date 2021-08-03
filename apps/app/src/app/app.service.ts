a800d886ebcd23c5c0fa3c545d55dd2626d3df5f
nontangent <irregular0number@gmail.com>
2021-08-03 22:52:06 +0900

    feat(app): add auth service

--- apps/app/src/app/app.service.ts
+++ apps/app/src/app/app.service.ts
@@ -4,9 +4,9 @@ import { TablePost, Team, UploadImageResponse } from '@esaiharamasukoi/api-inter
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Clipboard } from '@angular/cdk/clipboard';
-import { LoadingService, SheetsService, SnackBarService } from './_shared/services';
 import { Observable } from 'rxjs';
 
+import { AuthService, LoadingService, SheetsService, SnackBarService } from './_shared/services';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,7 @@ import { Observable } from 'rxjs';
 export class AppService {
   constructor(
     private http: HttpClient,
+    public auth: AuthService,
     public form: FormBuilder,
     public clipboard: Clipboard,
     public snackBar: SnackBarService,
