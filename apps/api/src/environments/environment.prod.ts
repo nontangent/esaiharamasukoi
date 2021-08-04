efe624e56b8859af09e67fc3e7fda40b1ccba21c
nontangent <irregular0number@gmail.com>
2021-08-04 16:52:43 +0900

    feat(api): change config name

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   production: false,
   origin: 'https://esaiharamasukoi.web.app',
   oauth: {
-    clientId: functions.config().key,
-    clientSecret: functions.config().secret,
+    clientId: functions.config().esa.id,
+    clientSecret: functions.config().esa.secret,
     callbackUrl: 'https://esaiharamasukoi.web.app/api/callback',
   },
 };
